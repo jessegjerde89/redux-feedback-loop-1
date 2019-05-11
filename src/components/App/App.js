c730da3ae2b9737fe7a0ad7a690a25f42f54ed63
heensaleem <heenakouser440@gmail.com>
2019-05-10 23:54:25 -0500

    setup route and components in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,13 +6,26 @@ import {connect} from 'react-redux'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Feedback!</h1>
-          <h4><i>Don't forget it!</i></h4>
-        </header>
-        <br/>
-      </div>
+      <Router>
+        <div className="App">
+          <Header className="App-header">
+             <h1>Feedback</h1>
+             <h2>Don't forget it!</h2>  
+          </Header>
+
+          <Route exact path='/feeling' 
+            component={Feeling}
+          />
+          <Route exact path='/understanding' 
+            component={Understanding}
+          />
+          <Route exact path='/support' component = {Support}  />
+          <Route exact path='/comments'
+            component={Comments}
+          />
+          <br/>
+        </div>
+      </Router>
     );
   }
 }
